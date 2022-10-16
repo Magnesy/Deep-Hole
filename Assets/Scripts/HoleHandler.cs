15284fb916f5492df5595c92bc85f5d36070286b
Magnesy <gunermahir1@gmail.com>
2022-10-16 23:15:16 +0300

    Up

---
+++ Assets/Scripts/HoleHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HoleHandler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
