38b664e465fc14cd02292ea6f1fe5d8ab737c188
Magnesy <gunermahir1@gmail.com>
2022-10-18 00:38:27 +0300

    Dynamic Joystick

--- Assets/Scripts/HoleHandler.cs
+++ Assets/Scripts/HoleHandler.cs
@@ -5,6 +5,11 @@ using UnityEngine.InputSystem;
 
 public class HoleHandler : MonoBehaviour
 {
+    public float speed;
+    public DynamicJoystick dynamicJoystick;
+    public Rigidbody rb;
+    [SerializeField] private float maxVelocity;
+    
     void Start()
     {
         
@@ -12,5 +17,15 @@ public class HoleHandler : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * dynamicJoystick.Vertical + Vector3.right * dynamicJoystick.Horizontal;
+        direction.y = 0f;
+        direction.Normalize();
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
 }
