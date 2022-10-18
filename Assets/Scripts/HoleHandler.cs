68f909dc6342b67644ba1ac7ba0813a8f9131a43
Magnesy <gunermahir1@gmail.com>
2022-10-19 00:58:57 +0300

    .

--- Assets/Scripts/HoleHandler.cs
+++ Assets/Scripts/HoleHandler.cs
@@ -9,23 +9,32 @@ public class HoleHandler : MonoBehaviour
     public FixedJoystick fixedJoystick;
     public Rigidbody rb;
     [SerializeField] private float maxVelocity;
-    
+
+
     void Start()
     {
         
     }
     void Update()
     {
-        
+
     }
 
 
     public void FixedUpdate()
     {
+        if(!Touchscreen.current.primaryTouch.press.isPressed){Invoke("StopMoving", 0.15f);}
         Vector3 direction = Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;
         direction.y = 0f;
         direction.Normalize();
         rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
+
+    public void StopMoving()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    
 }
