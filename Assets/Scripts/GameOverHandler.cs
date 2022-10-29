4dea0d8ba5b7e23c1f3d0babb5c62b816fc3394a
Magnesy <gunermahir1@gmail.com>
2022-10-30 02:08:41 +0300

    .

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -33,8 +33,8 @@ public class GameOverHandler : MonoBehaviour
         }
         else 
         {
-            PlayerPrefs.SetInt(LevelKey , level);
-            PlayerPrefs.SetInt(LastLevelKey , level);
+            PlayerPrefs.SetInt(LevelKey , 1);
+            PlayerPrefs.SetInt(LastLevelKey , 1);
             ReturnToMenu();
         }
     }
